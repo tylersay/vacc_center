de2883bab2a08634714957e0fc410e763fc775f1
Tyler Say <sayqi@live.com>
2021-12-01 11:00:46 +0800

    slot model up

--- vacc_app/models.py
+++ vacc_app/models.py
@@ -2,9 +2,14 @@ from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+
 class vac_Center(models.Model):
-  name = models.CharField(max_length=100)
-  vac_type = models.CharField(max_length=100)
+  center_name = models.CharField(max_length=100)
+  VAC_TYPE = (
+    ("P", "Pfizer"),
+    ("M", "Moderna")
+  )
+  vac_type = models.CharField(choices=VAC_TYPE)
   postal_code = models.IntegerField(
     default=1, 
     validators=[
@@ -12,4 +17,28 @@ class vac_Center(models.Model):
       MaxValueValidator(999999)
       ])
 
-class slots
+
+
+class slots(models.Model):
+  vac_center = models.ForeignKey(vac_Center, on_delete= models.CASCADE)
+  date_assigned = models.DateField()
+  TIME_SLOTS = (
+    ('9am', '9am'),
+    ('10am', '10am'),
+    ('11am', '11am'),
+    ('12am', '12am'),
+    ('1pm', '1pm'),
+    ('2pm', '2pm'),
+    ('3pm', '3pm'),
+    ('4pm', '4pm')   
+  )
+  time_slots = models.CharField(choices=TIME_SLOTS)
+  FIRST_OR_SECOND = (
+    ('First', 'First jab'),
+    ('Second', 'Second jab')
+  )
+  first_or_second = models.CharField(choices=FIRST_OR_SECOND)
+  person_name = models.CharField(max_length=100)
+  person_nric = models.CharField(max_length=4)
+
+  
